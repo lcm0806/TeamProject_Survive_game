db53e2b3378008d2c1a8410c39ab2b0040aaf67b
antactics <142508000+antactics@users.noreply.github.com>
2025-06-27 17:57:00 +0900

    [feat] panel map
    shelter, exit ui작업완료

--- Assets/WorkPlace/LHG/Scripts/ShelterUI.cs
+++ Assets/WorkPlace/LHG/Scripts/ShelterUI.cs
@@ -1,32 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using System;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 
 public class ShelterUI : MonoBehaviour
 {
     public StatusSystem StatusSystem;
+    [SerializeField] public GameObject SystemCanvas;
 
-    public GameObject[] ShelterMenu, Tabs, EventContainer, CompleteBtn;
+    public GameObject[] ShelterMenu, Tabs, EventContainer, CompleteBtn, MapLocations, ExitShelterCases;
     [SerializeField] public TMP_Text[] Indicators;
-    //public Image[] TabButtons;
-    //public Sprite InactiveTabBG, ActiveTabBG; // 활성 비활성 시각화를 백그라운드스프라이트로 처리할 때 필요
-    [SerializeField] public bool isEventCompleted;
-    [SerializeField] public bool canCompleteEvent;
+    public Image[] TabButtons, MapLocationsButtons;
+    public Sprite InactiveTabBG, ActiveTabBG; // 활성 비활성 시각화를 백그라운드스프라이트로 처리할 때 필요
+    
     [SerializeField] private EventState testState; //TODO 이벤트마다 스테이트를 가져야함, 충족시 해당 이벤트의 스테이트를 변경해주도록 만들어야
+
+    
+
 
     private void Start()
     {
         DisplayIndicators(0);
+        //시스템캔버스 false로 일단 
     }
     public void DisplayIndicators(int indicatorsID)
     {
-        Indicators[0].SetText($"Current Day :{StatusSystem.GetCurrentDay()}");
-        Indicators[1].SetText($"Current Oxygen : {StatusSystem.GetOxygen()}");
+        //[패널상단] 0:날짜 1:산소 / [패널]-[맵]-[쉘터]-[subUI] 2:산소, 3:전력(Energy/Electricity 용어통일필요?), 4:내구도
+        Indicators[0].SetText($"Day : {StatusSystem.GetCurrentDay()}");
+        Indicators[1].SetText($"Oxygen : {StatusSystem.GetOxygen()}");
+        Indicators[2].SetText($"Oxygen : {StatusSystem.GetOxygen()}");
+        Indicators[3].SetText($"Energy : {StatusSystem.GetEnergy()}");
+        Indicators[4].SetText($"Durability : {StatusSystem.GetDurability()}");
     }
 
     public void ActiveUI(int ShelterMenuID)
@@ -51,6 +60,9 @@ public class ShelterUI : MonoBehaviour
 
     public void SwitchToTab(int TabID)
     {
+        //Tabs [패널탭] 0:이벤트, 1:맵, 2:제작
+        //TabButtons(이미지변경용엘리먼트) [패널탭] 0:이벤트, 1:맵, 2:제작 
+
         EventCompleteBtnSwitcher(testState);
 
         foreach (GameObject go in Tabs)
@@ -59,14 +71,15 @@ public class ShelterUI : MonoBehaviour
         }
         Tabs[TabID].SetActive(true);
 
-        //foreach (Image im in TabButtons)
-        //{
-        //    im.sprite = InactiveTabBG;
-        //}
-        //TabButtons[TabID].sprite = ActiveTabBG;
+        foreach (Image im in TabButtons)
+        {
+            im.sprite = InactiveTabBG;
+        }
+        TabButtons[TabID].sprite = ActiveTabBG;
     }
 
-    
+
+    //패널-이벤트-서브ui이벤트리스트를 누르면 해당 메인ui가 액티브
     public void SelectEventList(int EventContainerID)
     {
         foreach (GameObject go in EventContainer)
@@ -75,6 +88,104 @@ public class ShelterUI : MonoBehaviour
         }
         EventContainer[EventContainerID].SetActive(true);
     }
+
+
+    //패널-맵- 침실,쉘터,출구를 선택하면 해당 서브ui가 액티브
+    public void SelectMapList(int MapLocationsID)
+    {
+        //MapLocations 0:침실, 1:쉘터, 2,출구
+        foreach (GameObject go in MapLocations)
+        {
+            go.SetActive(false);
+        }
+        MapLocations[MapLocationsID].SetActive(true);
+
+        //[패널-지도탭] 0:침실 1: 쉘터 2:출구
+        foreach (Image im in MapLocationsButtons)
+        {
+            im.sprite = InactiveTabBG;
+        }
+        MapLocationsButtons[MapLocationsID].sprite = ActiveTabBG;
+
+    }
+
+   
+    
+    public void ExitShelter()
+    {
+        //시스템캔버스를 active해줘야함
+        Debug.Log("출구 버튼눌림");
+        if(SystemCanvas.activeSelf == false) //activeSelf이부분살펴보기
+        {
+            SystemCanvas.SetActive(true);
+            Debug.Log("시스템캔버스 액티브");
+
+            //ID 0:100이넘는경우ui창, 1:100미만인경우ui창, 2:오늘이미탐색한경우ui창
+            //오늘 탐색여부를 확인
+            //false라면
+            if (StatusSystem.GetIsToDay() == false)
+            {
+                Debug.Log("오늘 탐색가능");
+                // 산소보유량이 100이 넘는 경우
+                if (StatusSystem.GetOxygen() >= 100)
+                {
+                    Debug.Log("보유산소 100이상");
+                    foreach (GameObject go in ExitShelterCases)
+                    {
+                        Debug.Log("기존ui비활성");
+                        go.SetActive(false);
+                    }
+                    ExitShelterCases[0].SetActive(true);
+                    //확인ui을 띄움
+                    //실행클릭시 산소를 100소모하고 씬전환
+                    //취소클릭시 창닫기
+
+                }
+                // 산소보유량이 100미만인 경우(=산소부족시)
+                else
+                {
+                    Debug.Log("보유산소 100미만");
+                    foreach (GameObject go in ExitShelterCases)
+                    {
+                        go.SetActive(false);
+                    }
+                    ExitShelterCases[1].SetActive(true);
+                    //확인ui을 띄우고
+                    //실행클릭시 남은 모든 산소를 소모하고 씬전환
+                    //취소클릭시 창닫기
+
+                }
+                //만약 산소가 0인..경우?
+            }
+            // 오늘 이미 탐색했다면 
+            else
+            {
+                Debug.Log("오늘 이미 탐색");
+                foreach (GameObject go in ExitShelterCases)
+                {
+                    go.SetActive(false);
+                }
+                ExitShelterCases[2].SetActive(true);
+            }
+        }
+
+
+        
+
+
+
+
+        //true라면
+
+        //1-1 산소 100이 넘는 경우-> 팝업창 예(산소100깎고, 씬체인지) 아니오(창닫기)
+
+        //1-2 산소 100 미만인 경우 -> 팝업창 예(산소 {현재량} 깎고, 씬체인지) 아니오(창닫기)
+
+
+        //2. 이미 탐색을 한 경우 탐색 불가 -> 팝업창 (오늘은 이미 탐색을 끝냈습니다) 확인(창닫기)
+
+    }
+    
 
     public void EventCompleteBtnSwitcher(EventState state)
     {

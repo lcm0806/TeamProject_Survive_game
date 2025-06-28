7fe848401edd5a92eac5ae724f9347f0400b0490
lcm0806 <82375404+lcm0806@users.noreply.github.com>
2025-06-28 09:18:23 +0900

    [FIX] 아이템 인벤토리 생성 버그 수정
    아이템을 오브젝트 형식일때 획득후 인벤토리 추가할때 손에 아이템을 들고 있다면 손에 있는 아이템을 획득하는 버그를 수정

--- Assets/WorkPlace/LCM/Scripts/ScriptableObject/ToolItem.cs
+++ Assets/WorkPlace/LCM/Scripts/ScriptableObject/ToolItem.cs
@@ -9,11 +9,15 @@ public class ToolItem : Item
     [Header("Tool Specific Info")]
     public float miningPower = 1f; // 채굴 도구의 채굴력 (예: 광물의 체력을 깎는 양)
     public ToolType toolType;       // 이 도구의 종류 (예: 곡괭이, 도끼, 삽 등)
+    public GameObject toolPrefab;     // 플레이어 손에 들릴 오브젝트 프리팹
+    public GameObject toolObject;     // 도구의 실제 오브젝트 (예: 곡괭이, 도끼 등)
+    public ToolAction toolAction;
 
     public override void Use(GameObject user)
     {
         // 부모 Item 클래스의 Use 메소드를 호출합니다.
-        base.Use(user); // 디버깅 용
+        /*base.Use(user);*/ // 디버깅 용
+        toolAction.Action((int)miningPower);
 
         Debug.Log($"{itemName}을 사용했습니다. (내구도 없음)");
 

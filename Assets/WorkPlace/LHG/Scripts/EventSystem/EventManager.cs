eaab2515b2062567ee09622db2862e17a5a7bc15
antactics <142508000+antactics@users.noreply.github.com>
2025-06-30 17:55:51 +0900

    [feat] craft tab ui, event system on work
    제작탭 ui제작하다가 중단.. 이벤트 시스템 구조 설계중

---
+++ Assets/WorkPlace/LHG/Scripts/EventSystem/EventManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    //전체 퀘스트 시스템을 관리하는 중심 역할을 합니다. 퀘스트 데이터 로드, 퀘스트 활성화, 진행 상태 업데이트 등을 담당합니다.
+    
+    //인벤토리 및 창고 
+
+    // 이벤트 데이터 목록을 가져오고
+    // 이벤트 발생
+
+    
+
+    // 이벤트 진행상태판별
+
+    // 이벤트 완료처리
+
+
+}
---
+++ Assets/WorkPlace/LHG/Scripts/EventSystem/EventUI.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventUI : MonoBehaviour
+{
+   //패널, 버튼, 스크롤 뷰 등 ui연동
+}

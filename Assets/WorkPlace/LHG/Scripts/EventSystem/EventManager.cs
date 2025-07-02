5c85395e801a22fece11bc1d95bb4804238f3f8b
antactics <142508000+antactics@users.noreply.github.com>
2025-07-02 12:29:54 +0900

    [feat] event list update
    게임 후기 이벤트 리스트 반영

--- Assets/WorkPlace/LHG/Scripts/EventSystem/EventManager.cs
+++ Assets/WorkPlace/LHG/Scripts/EventSystem/EventManager.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//전체 이벤트 시스템을 관리, 이벤트 데이터 로드, 이벤트 활성화, 진행 상태 업데이트, 이벤트완료 등
 public class EventManager : MonoBehaviour
 {
-    //전체 이벤트 시스템을 관리, 이벤트 데이터 로드, 이벤트 활성화, 진행 상태 업데이트, 이벤트완료 등
-    
     public static EventManager Instance { get; private set; }
     private Dictionary<int, GameEventData> eventDict = new();
     public List<GameEventData> allEvents = new();
+    public List<GameEventData> onWorkingEvents = new();
+
 
     private int _curGameDay = StatusSystem.Instance.GetCurrentDay();
 
@@ -23,13 +24,13 @@ public class EventManager : MonoBehaviour
         LoadAllEvents();
     }
 
-    // 이벤트 데이터 목록을 가져오고
+    // 모든 이벤트 데이터 목록을 가져오고
     private void LoadAllEvents()
     {
         var events = Resources.LoadAll<GameEventData>("Events");
-        foreach(var e in events)
+        foreach (var e in events)
         {
-            if(!eventDict.ContainsKey(e.id))
+            if (!eventDict.ContainsKey(e.id))
             {
                 eventDict[e.id] = e;
                 allEvents.Add(e);
@@ -48,37 +49,58 @@ public class EventManager : MonoBehaviour
         return result;
     }
 
+    //현재 발동된 이벤트들의 리스트 있어야 할듯 ? onWorkingEvents
     
 
+
+
+    // 이벤트가 발동 또는 생성되는 로직
     public void TriggerEvent()
     {
-        if (GameEventData.Instance.reactiveAfterEnd==true)
+        // 매일 아침 현재 발동중인 이벤트를 전체를 살펴보고
+        // 오늘이 며칠째인지 판별하고
+        // 일자에 맞는 이벤트id범위의 SO에서 랜덤하게 발동
+
+        // 단, 중복되는 id는 발동시키지 않음
+
+        // 단, 게임 시작부터 매일 재생성되는 이벤트 id 10001
+        if (GameEventData.Instance.reactiveAfterEnd == true)
         {
 
         }
-        //1일차에
 
+        //1~3일차에 id 10002~10004 중 1개
 
+        //4~6일차에 id10002~10006 중 2개
 
-        //1~3일차에
-        
+        //7일차~ id10005~10009 중 2개
 
-
-        //4일차~ 
     }
 
-   
-
-    
-
-
-    // 이벤트 발생
 
 
 
-    // 이벤트 진행상태판별
+    // 이벤트가 완료가능인지 판별
+    public void DetermineEventComplete()
+    {
 
-    // 이벤트 완료처리
+        // if required item count < 인벤토리의 cur_item_count이면 item count --해주고
+        // 해당 id의 이벤트 자체를 제거
+
+    }
+
+
+    // 
+
+
+
+
+
+
+
+
+
+
 
 
 }
--- Assets/WorkPlace/LHG/Scripts/EventSystem/GameEventData.cs
+++ Assets/WorkPlace/LHG/Scripts/EventSystem/GameEventData.cs
@@ -3,30 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//모든 퀘스트 데이터를 저장
+//모든 게임 이벤트들의 데이터
 
 
 [CreateAssetMenu(fileName = "GameEventData", menuName = "Event System/Game Event")]
-public class GameEventData :ScriptableObject
+public class GameEventData : ScriptableObject
 {
+    
+
+
     public static GameEventData Instance { get; private set; }
 
     public int id;
     public string title;
     [TextArea] public string description;
-    
+
 
     [Header("이벤트 효과(밤마다 발동되는 불이익)")]
-    [TextArea] public string effectDescription;
+    public double minusCurDurability;
+    public int curOxygen;
+    public int curEnergy;
+    public int BatteryEfficiency; 
+    public int OxygenEfficiency;
 
     [Header("종료 조건(필요 아이템 등)")]
-    public string requiredItem;
-    public int requiredAmount;
+    public string requiredItem; //요구 아이템이 두가지 이상인데..어떻게하지
+    public int requiredAmount; 
 
-    [Header("종료 후 처리(보상 또는 회복)")]
-    [TextArea] public string endEffectDescription;
+    [Header("첫날 + 매일 기본적으로 재생성되는 이벤트")]
     public bool reactiveAfterEnd;
 
-    [Header("미완료 날짜전환시 출력 대사")]
+    [Header("미완료상태로 날짜전환시 출력 대사")]
     [TextArea] public string dialogue;
 }
